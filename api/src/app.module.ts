1f52dc7d03cbb7001ee7cd3739a3ec8a73720551
Felipe Falcone <lpmaggots@gmail.com>
2025-08-05 15:32:19 -0300

    feat: add initial modules for user, account, transaction and institution

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { InstitutionsModule } from './institutions/institutions.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, AccountsModule, TransactionsModule, InstitutionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
