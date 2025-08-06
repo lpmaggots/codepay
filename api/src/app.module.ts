f5a0573db27829b09f195a77cadf7e88f5ca62b9
Felipe Falcone <lpmaggots@gmail.com>
2025-08-06 14:04:36 -0300

    feat: use cache module as global

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,15 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { AccountsModule } from './accounts/accounts.module';
-import { TransactionsModule } from './transactions/transactions.module';
-import { InstitutionsModule } from './institutions/institutions.module';
-import { PrismaModule } from '@/prisma/prisma.module';
-
+import { Module } from '@nestjs/common'
+import { CacheModule } from '@nestjs/cache-manager'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { AuthModule } from './auth/auth.module'
+import { UsersModule } from './users/users.module'
+import { AccountsModule } from './accounts/accounts.module'
+import { TransactionsModule } from './transactions/transactions.module'
+import { InstitutionsModule } from './institutions/institutions.module'
+import { PrismaModule } from '@/prisma/prisma.module'
 @Module({
-  imports: [AuthModule, UsersModule, AccountsModule, TransactionsModule, InstitutionsModule, PrismaModule],
+  imports: [
+    CacheModule.register({
+      isGlobal: true
+    }),
+    AuthModule,
+    UsersModule,
+    AccountsModule,
+    TransactionsModule,
+    InstitutionsModule,
+    PrismaModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
