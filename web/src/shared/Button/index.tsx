8f95d6e7ae9945db6b4377da1b799b2d71741faf
Felipe Falcone <lpmaggots@gmail.com>
2025-08-14 17:01:23 -0300

    feat: add type link to button

--- web/src/shared/Button/index.tsx
+++ web/src/shared/Button/index.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode
-  variant?: 'primary' | 'secondary' | 'floating',
+  variant?: 'primary' | 'secondary' | 'floating' | 'link',
   className?: string
 }
 
@@ -14,6 +14,7 @@ export default function Button({ children, variant = 'primary', className, ...pr
     primary: 'py-2 px-4 rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 border-transparent',
     secondary: 'py-2 px-4 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:ring-blue-500 border border-gray-300',
     floating: 'fixed bottom-6 right-6 bg-blue-600 hover:bg-blue-700 text-white rounded-full p-4 shadow-lg focus:ring-blue-500 transform hover:scale-110 text-lg font-semibold',
+    link: 'text-blue-600 hover:text-blue-800 text-xs font-medium',
   }
 
   return (
